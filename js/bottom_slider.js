36105a6b69b57aa8e2643262656d79c112672cf5
Deni4ka <atamaniukdenysv@gmail.com>
2025-01-26 18:02:32 +0200

    appdate services page 4

--- js/bottom_slider.js
+++ js/bottom_slider.js
@@ -1,16 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const main__slider = new Swiper('.recent__projects-slider', {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 80,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    on: {
-      init: function () {
-        console.log('Swiper ініціалізовано');
-      }
+  function checkWindowWidth() {
+    const width = window.innerWidth;
+
+    if (width < 1200) {
+      console.log('Скрипт не буде виконуватись на екранах менше ніж 1200px');
+      return;
     }
-  });
+    const main__slider = new Swiper('.recent__projects-slider', {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 35,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        1600: {
+          spaceBetween: 80,
+        }
+      },
+      on: {
+        init: function () {
+          console.log('Swiper ініціалізовано');
+        }
+      }
+    });
+  }
+
+  checkWindowWidth();
+
+  window.addEventListener('resize', checkWindowWidth);
 });
+
